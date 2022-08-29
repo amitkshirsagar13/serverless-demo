f045a0781d55d1202c7c700775c7ba51d37b43b3
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2022-08-29 11:11:09 +0530

    first commit

---
+++ handlers/sqs/tenantHandler.ts
@@ -0,0 +1,17 @@
+import { Context, Handler, Callback } from 'aws-lambda';
+
+/**
+ * @description handle some tank payload
+ * @param event event object
+ * @param context context object
+ */
+const run: Handler = async (event: any, context: Context, callback: Callback): Promise<any> => {
+  const shot = {
+    message: 'Boom!!! 💥 Boom!!! 💥',
+    event,
+    exitCode: 0
+  };
+  callback(undefined, { "statusCode" : 200, "body" : shot});
+};
+
+export { run };
